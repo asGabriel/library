0956dbf059d2398e4a0610e962a95269782540b0
asGabriel <andrade.gabriel2295@gmail.com>
2024-03-01 06:44:04 -0300

    feat(books): add route get book by id

--- src/handlers/books.rs
+++ src/handlers/books.rs
@@ -1,6 +1,8 @@
+use uuid::Uuid;
+
 use crate::domain::{
     books::{Book, CreateBook},
-    errors::Result,
+    errors::{Error, Result},
 };
 
 use super::Handler;
@@ -8,5 +10,12 @@ use super::Handler;
 impl Handler {
     pub async fn create_book(&self, book: CreateBook) -> Result<Book> {
         self.book_repository.create_book(book).await
+    }
+
+    pub async fn get_book_by_id(&self, book_id: Uuid) -> Result<Book> {
+        self.book_repository
+            .get_book_by_id(book_id)
+            .await?
+            .ok_or(Error::BookNotFound(book_id))
     }
 }
