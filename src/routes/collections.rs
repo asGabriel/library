8cb1e7db9b344fa968ea4947a1e0b56ba8d9d523
asGabriel <andrade.gabriel2295@gmail.com>
2024-03-13 06:32:23 -0300

    feat(collections): add create collection route

--- src/routes/collections.rs
+++ src/routes/collections.rs
@@ -1,7 +1,16 @@
-use axum::Router;
+use axum::{extract::State, response::IntoResponse, routing::post, Json, Router};
 
-use crate::handlers::Handler;
+use crate::{domain::{collections::CreateCollection, errors::Result}, handlers::Handler};
 
 pub(super) fn configure_routes() -> Router<Handler> {
-    Router::new().nest("/collections", Router::new())
+    Router::new().route("/collections", post(create_collection))
+}
+
+async fn create_collection(
+    State(handler): State<Handler>,
+    Json(collection): Json<CreateCollection>
+) -> Result<impl IntoResponse> {
+    let collection = handler.create_collection(collection).await?;
+
+    Ok(Json::from(collection))
 }
